82af5e14284b699302b19b6ea43130d427cdda6b
Bilal Sadiq <bnaq860@gmail.com>
2026-02-03 15:40:27 +0530

    fix: correct IsAuthenticated import

--- backend/community/views.py
+++ backend/community/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view, permission_classes, authentication_classes, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import  AllowAny
-from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import  AllowAny, IsAuthenticated
 from .serializers import UserSerializer
 from django.db.models import Count, Sum
 from .models import Posts, Comment, Like, KarmaTransaction
